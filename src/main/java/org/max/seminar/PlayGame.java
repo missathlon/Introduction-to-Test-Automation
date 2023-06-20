a8719518186fcd4af2f43ed943057e63f92003be
kravmaxim@gmail.com <Cegthgfhjkm!666>
2023-06-20 09:46:15 +0300

    -

--- src/main/java/org/max/seminar/PlayGame.java
+++ src/main/java/org/max/seminar/PlayGame.java
@@ -1,7 +1,9 @@
 package org.max.seminar;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 
 /**
  * Main класс для запуска BlackJack в цикле
@@ -10,17 +12,21 @@ public class PlayGame {
 
     public static void main(String[] args) {
 
-        Gamer gamer = new Gamer(7);
-        Casino casino = new Casino(0);
+
         HashMap<String, String> statistic = new LinkedHashMap<>();
+        List<String> winnerName = new ArrayList<>();
 
         for (int i = 0; i<100; i++) {
+            Gamer gamer = new Gamer(3);
+            Casino casino = new Casino(0);
             Game game = new Game(gamer, casino);
-
-            statistic.put("Game № " + i, game.round());
+            String winner = game.round();
+            winnerName.add(winner);
+            statistic.put("Game № " + i + 1, winner);
         }
 
         statistic.entrySet().forEach(System.out::println);
+        System.out.println("Игрок выиграл: " + winnerName.stream().filter(v -> v.equals("Игрок")).count());
 
     }
 
